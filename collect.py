07b88eec2b27980f232cc55e7cc4dbac43c9248d
纯真浩一郎 <72e1bcgg@gmail.com>
2026-07-16 11:26:38 +0800

    fix: update logging configuration to ensure log messages are written to log.txt

--- collect.py
+++ collect.py
@@ -10,6 +10,7 @@ def main():
         level=logging.INFO,
         format="%(asctime)s %(levelname)s %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
+        handlers=(logging.StreamHandler(), logging.FileHandler("log.txt")),
     )
     threads = (
         threading.Thread(
